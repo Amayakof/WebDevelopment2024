871a91d245c2d253f10e4ac81fc584fa12da39b1
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-03-22 13:11:09 +0500

    Informatics a

---
+++ Lab7/Informatics/a.py
@@ -0,0 +1,39 @@
+import math
+
+# a. Гипотенуза
+# a = int(input("Enter side a: "))
+# b = int(input("Enter side b: "))
+# c = float(math.sqrt(a**2 + b**2))
+# print("The length of hypothenuse is: ", c)
+
+# b. Следующее и предыдущее
+# n = int(input())
+# print(f"The next number for the number {n} is {n+1}.")
+# print(f"The previous number for the number {n} is {n-1}.")
+
+# c. Дележ яблок - 1
+# n = int(input())
+# k = int(input())
+# print(int(k/n))
+
+# d. Дележ яблок - 2
+# n = int(input())
+# k = int(input())
+# print(round(k % n))
+
+# e. МКАД
+# v = int(input())
+# t = int(input())
+# dist = v * t
+# if v > 0:
+#     pos = dist % 109
+# elif v < 0:
+#     pos = (109 + dist) % 109
+# else:
+#     pos = 0
+#
+# print(pos)
+
+# f. Последняя цифра
+# n = int(input())
+# print("Last digit:", n % 10)
