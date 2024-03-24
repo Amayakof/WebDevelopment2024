30b328fad1d657b82b903b9a26c798a98c06b1bf
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-03-24 12:53:04 +0500

    Informatics cWhile

---
+++ Lab7/Informatics/cWhile.py
@@ -0,0 +1,45 @@
+import math
+# a. Список квадратов
+# n = int(input("Enter a number: "))
+# i = 1
+# while i * i <= n:
+#     print(i ** 2)
+#     i += 1
+
+# b. Минимальный делитель
+# n = int(input("Enter a number: "))
+# divider = 2
+# while n % divider != 0:
+#     divider += 1
+#
+# print(divider)
+
+# c. Список степеней двойки
+# number = int(input("Enter a number: "))
+# power = 1
+# while power <= number:
+#     print(power, end=" ")
+#     power *= 2
+
+# d. Точная степень двойки
+# number = int(input("Enter a number: "))
+# if number == 1:
+#     print("YES")
+# else:
+#     while number > 1:
+#         if number % 2 != 0:
+#             print("NO")
+#             break
+#         number //= 2
+#     else:
+#         print("YES")
+
+# e. Двоичный логарифм
+# number = int(input("Enter a number: "))
+# k = 0
+# power = 1
+# while power < number:
+#     power *= 2
+#     k += 1
+# print(k)
+
