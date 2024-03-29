e191846051bd645baf8a2197dd3408c81803fd6c
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-03-29 10:15:22 +0500

    Task2

--- Lab7/CodingBat/List2/sum13.py
+++ Lab7/CodingBat/List2/sum13.py
@@ -1,4 +1,5 @@
-# Return the sum of the numbers in the array, 
+from typing import List
+# Return the sum of the numbers in the array,
 # returning 0 for an empty array.
 # Except the number 13 is very unlucky,
 # so it does not count and numbers that come immediately after a 13 also do not count.
@@ -33,7 +34,7 @@ def sum13(nums):
 #     return total
 
 
-
 if  __name__ == '__main__':
-    nums = [13, 1, 2, 3]
+    nums: List[int] = [13, 1, 2, 3]
     print(sum13(nums))
+    
