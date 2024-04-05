b637a721ff55c8b4a3f999bf109da99a9662e9b0
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-04-05 10:26:39 +0500

    upd

--- Labb8/shopback/api/admin.py
+++ Labb8/shopback/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category, ProductCategory
+# Register your models here.
 
+from .models import Product, Category, ProductCategory
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(ProductCategory)
-# Register your models here.
+
